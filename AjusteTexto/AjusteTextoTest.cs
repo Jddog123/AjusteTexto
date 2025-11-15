6a1a45b641b8b8962d900688ce124bbe6cf10e58
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-14 19:30:09 -0500

    🔴e

--- AjusteTexto/AjusteTextoTest.cs
+++ AjusteTexto/AjusteTextoTest.cs
@@ -35,6 +35,14 @@ public class AjusteTextoTest
 
         result.Should().Be("abc\ndef\nghi\nj");
     }
+    
+    [Fact]
+    public void e()
+    {
+        var result = Wrap("word word", 3);
+
+        result.Should().Be("wor\nd\nwor\nd");
+    }
 
     private static string Wrap(string text, int col)
     {
