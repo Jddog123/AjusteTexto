709dd51876b4e14dd200322c9836d7f1c2001b7f
Jddog123 <daniel.uruena@sinco.com.co>
2025-11-14 19:16:24 -0500

    🔴a

--- AjusteTexto/AjusteTextoTest.cs
+++ AjusteTexto/AjusteTextoTest.cs
@@ -1,9 +1,19 @@
-﻿namespace AjusteTexto;
+﻿using FluentAssertions;
+
+namespace AjusteTexto;
 
 public class AjusteTextoTest
 {
     [Fact]
-    public void Test1()
+    public void a()
     {
+        var result = Wrap("", 1);
+
+        result.Should().Be("");
+    }
+
+    private static string Wrap(string text, int col)
+    {
+        throw new Exception();
     }
 }
